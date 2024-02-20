05fa94b7ff0d932a4bf4eb243356a15450144492
AyoubNaitaLI <naitaliayoub4@gmail.com>
2024-02-20 20:20:52 +0000

    commit

---
+++ test/test_file_storage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is the test_file_storage module
+for testing the file_storage class
+"""
+
+
+import unittest
+from ...models.engine.file_storage import FileStorage
+
+
+class TestFileStorageClass(unittest.TestCase):
+    """ This is the TestFileStorageClass class """
+    pass
